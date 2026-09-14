d4a7df95b43cc93f72888e9dce433a11558cb489
khonoilam <hahuytien19@gmail.com>
2026-09-14 12:54:47 +0700

    Change BOT_TOKEN in cloud_bot.py
    Updated BOT_TOKEN to a new value.

--- cloud_bot.py
+++ cloud_bot.py
@@ -27,7 +27,7 @@ from telegram.ext import (
 # ==================================================================
 # CẤU HÌNH
 # ==================================================================
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8988947106:AAEQxdenSha6W_5De4NMEnDoc5-RSvMkenk")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8988947106:AAHFY43SQLRP7Fkcgo2-a4x5rR3HyyDnhTw")
 ADMIN_ID = 7267437767
 GROUP_ID = -1004318229096
 GROUP_LINK = "https://t.me/cloudfreeaot"
